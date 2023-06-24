adf1bfa74c225995431b7c4f96e08f877b4936b7
TakshDhabalia <dhabalia.taksh@gmail.com>
2023-06-25 02:08:04 +0530

    minor addition , dont bother

---
+++ LSTM/LSTM.py
@@ -0,0 +1,16 @@
+#we now need to dive deeper into LSTM and their structure 
+"""
+Long short term Memory or LSTM -- 
+what my understanding is ---->
+                                1.recurrent neural network
+                                2.trained using back propogation 
+                                3.solves the problme of gradient vanishing 
+                                4.we use neurons traditionally but here we use what we call a memory block in layer 
+type of a recurrent neural network with some more variables in them 
+
+Gates used in a LSTM --->
+
+
+
+
+"""
