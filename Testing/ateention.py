f44b53c783cae2b693b1d9ec164cb151f2f0f183
TakshDhabalia <dhabalia.taksh@gmail.com>
2023-06-28 01:03:53 +0530

    Added Plan sources and description clarity

---
+++ Testing/ateention.py
@@ -0,0 +1,4 @@
+"""
+attention from video is a small subsection of a larger problme statement and this is just one of the attempts of implemeting the attention (in the form of still images or ) 
+Probably going to go with google colab for the experimental part but i will put the prod code here for my reference too !
+"""
